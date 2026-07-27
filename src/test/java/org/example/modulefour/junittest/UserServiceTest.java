02dd9a0284e9101bb48577d74f180bb7e5aa4a11
Nikita Shurygin <nekit.com2016@mail.ru>
2026-07-27 15:11:03 +0300

    Реализованы юнит тесты для всех методов

--- src/test/java/org/example/modulefour/junittest/UserServiceTest.java
+++ src/test/java/org/example/modulefour/junittest/UserServiceTest.java
@@ -15,13 +15,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -53,16 +54,20 @@ public class UserServiceTest {
 
         assertNotNull(resultUser);
         assertEquals(user.getName(), resultUser.getName());
+        verify(userRepository).findById(id);
     }
 
     @Test
-    void getUserTest_NotExists() {
+    void getUserTestNotExists() {
         Long id = 1L;
         when(userRepository.findById(id)).thenReturn(Optional.empty());
 
         ResponseEntity<?> result = userService.getUser(id);
+        Map<String, String> body = (Map<String, String>) result.getBody();
 
         assertEquals(HttpStatus.NOT_FOUND, result.getStatusCode());
+        assertThat(body.get("error")).isEqualTo(ErrorsMessages.USER_NOT_FOUND.getMessage());
+        verify(userRepository).findById(id);
     }
 
     @Test
@@ -76,6 +81,8 @@ public class UserServiceTest {
 
         assertEquals(user.getName(), result.get(0).getName());
         assertEquals(user2.getName(), result.get(1).getName());
+        verify(userRepository).findAll();
+
     }
 
     @Test
@@ -89,5 +96,74 @@ public class UserServiceTest {
 
         assertEquals(userDTO.getName(), result.getName());
         assertEquals(userDTO.getEmail(), result.getEmail());
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void deleteUserTest() {
+        User user = initializeUser(0);
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        ResponseEntity<?> response = userService.deleteUser(user.getId());
+        UserDTO resultDTO = (UserDTO) response.getBody();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(user.getName(), resultDTO.getName());
+        assertThat(response.getBody()).isInstanceOf(UserDTO.class);
+        verify(userRepository).findById(user.getId());
+        verify(userRepository).deleteById(user.getId());
+    }
+
+    @Test
+    void deleteUserTestNotExists() {
+        Long id = 99L;
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+        ResponseEntity<?> response = userService.deleteUser(id);
+        Map<String, String> body = (Map<String, String>) response.getBody();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isInstanceOf(Map.class);
+        assertThat(body.get("error")).isEqualTo(ErrorsMessages.USER_NOT_FOUND.getMessage());
+        verify(userRepository).findById(id);
+        verify(userRepository, never()).deleteById(any());
+    }
+
+    @Test
+    void updateUserTest() {
+        User user = initializeUser(0);
+        Long id = user.getId();
+        UserCreateDTO userCreateDTO = initializeUserCreateDTO(1);
+        User updatedUser = initializeUser(1);
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        when(userRepository.save(any(User.class))).thenReturn(updatedUser);
+
+        ResponseEntity<?> response = userService.updateUser(id, userCreateDTO);
+        UserDTO body = (UserDTO) response.getBody();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(user.getName(), body.getName());
+        assertEquals(user.getEmail(), body.getEmail());
+        verify(userRepository).findById(id);
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void updateUserTestNotExists() {
+        Long id = 99L;
+        UserCreateDTO userCreateDTO = initializeUserCreateDTO(1);
+
+        when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+        ResponseEntity<?> response = userService.updateUser(id, userCreateDTO);
+        Map<String, String> body = (Map<String, String>) response.getBody();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isInstanceOf(Map.class);
+        assertThat(body.get("error")).isEqualTo(ErrorsMessages.USER_NOT_FOUND.getMessage());
+        verify(userRepository).findById(id);
+        verify(userRepository, never()).save(any());
     }
 }
