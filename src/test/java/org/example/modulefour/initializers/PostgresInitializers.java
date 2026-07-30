5028f38632dc1f8696d361068f4f27fa1120d12d
Nikita Shurygin <nekit.com2016@mail.ru>
2026-07-30 22:21:09 +0300

    MockMVC тесты для UserController

---
+++ src/test/java/org/example/modulefour/initializers/PostgresInitializers.java
@@ -0,0 +1,31 @@
+package org.example.modulefour.initializers;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Testcontainers
+public class PostgresInitializers {
+    @Container
+    protected static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @BeforeAll
+    static void setUpContainer() {
+        System.setProperty("spring.datasource.url", POSTGRE_SQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTGRE_SQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", POSTGRE_SQL_CONTAINER.getPassword());
+        System.setProperty("spring.jpa.hibernate.ddl-auto", "create-drop");
+    }
+
+    @AfterAll
+    static void tearDownContainer() {
+        if (POSTGRE_SQL_CONTAINER != null) {
+            POSTGRE_SQL_CONTAINER.stop();
+        }
+    }
+}
---
+++ src/test/java/org/example/modulefour/mockmvctest/UserControllerTest.java
@@ -0,0 +1,108 @@
+package org.example.modulefour.mockmvctest;
+
+import org.example.modulefour.controllers.UserController;
+import org.example.modulefour.domain.dto.UserCreateDTO;
+import org.example.modulefour.domain.entities.User;
+import org.example.modulefour.initializers.PostgresInitializers;
+import org.example.modulefour.repositories.UserRepository;
+import org.example.modulefour.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureWebMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class UserControllerTest extends PostgresInitializers {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserCreateDTO initializeUserCreateDTO(Integer num) {
+        UserCreateDTO dto = new UserCreateDTO("TestName" + num, "TestEmail" + num, 10 + num);
+        return dto;
+    }
+
+    public User initializeUser(UserCreateDTO dto) {
+        User user = new User(dto);
+        return user;
+    }
+
+
+    @Test
+    public void getUserTest() throws Exception {
+        User user = initializeUser(initializeUserCreateDTO(0));
+        userRepository.save(user);
+
+        mockMvc.perform(get("/getUser/" + user.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(user.getName()));
+
+    }
+
+    @Test
+    public void createUserTest() throws Exception {
+        UserCreateDTO dto = initializeUserCreateDTO(0);
+
+        mockMvc.perform(post("/createUser")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(dto.getName()));
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception {
+        User user = initializeUser(initializeUserCreateDTO(0));
+        userRepository.save(user);
+
+        mockMvc.perform(delete("/deleteUser/" + user.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(user.getName()));
+    }
+
+    @Test
+    public void updateUserTest() throws Exception {
+        UserCreateDTO dto = initializeUserCreateDTO(1);
+        User user = initializeUser(initializeUserCreateDTO(0));
+        userRepository.save(user);
+
+        mockMvc.perform(put("/updateUser/" + user.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(dto.getName()));
+    }
+
+    @Test
+    public void getAllUsersTest() throws Exception{
+        User user = initializeUser(initializeUserCreateDTO(0));
+        User user2 = initializeUser(initializeUserCreateDTO(1));
+        userRepository.saveAll(List.of(user, user2));
+
+        mockMvc.perform(get("/allUsers"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value(user.getName()))
+                .andExpect(jsonPath("$[1].name").value(user2.getName()));
+    }
+}
